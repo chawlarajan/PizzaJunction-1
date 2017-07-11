a89dd371c4758e6819bad2f26a4dc49c5986ea17
Gurpreet Singh <gopisivia@gmail.com>
2017-07-11 16:37:38 +0530

    Added code for angular maps

--- client/src/app/contact-us/contact-us.component.ts
+++ client/src/app/contact-us/contact-us.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './contact-us.component.html'
+    templateUrl: './contact-us.component.html',
+    styles: [`
+        .agm-map { height: 300px; }
+    `]
 })
-export class ContactUs { }
+export class ContactUs {
+    private title: string = 'Pizza Junction Snacks';
+    private lat: number = 49.054765;
+    private lng: number = -122.325902;
+}
--- client/src/app/contact-us/contact-us.module.ts
+++ client/src/app/contact-us/contact-us.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ContactUs } from './contact-us.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AgmCoreModule } from '@agm/core';
 
-const contactUsRoutes: Routes = [{ path:'', component: ContactUs}];
+const contactUsRoutes: Routes = [{ path: '', component: ContactUs }];
 
 @NgModule({
     imports: [
-        RouterModule.forChild(contactUsRoutes)
+        RouterModule.forChild(contactUsRoutes),
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyDQVCEwiDngg-HOyOD8-TskXQywkmCi-Rw'
+        })
     ],
     declarations: [
         ContactUs
