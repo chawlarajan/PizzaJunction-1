70af79e7472572c16cfc76feff07cc88e67e6a60
Gurpreet Singh <gopisivia@gmail.com>
2017-07-12 10:03:44 +0530

    Fixed issue with map on ContactUs

--- client/src/app/contact-us/contact-us.component.ts
+++ client/src/app/contact-us/contact-us.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     templateUrl: './contact-us.component.html',
     styles: [`
-        .agm-map { height: 300px; }
+        agm-map { height: 300px; }
     `]
 })
 export class ContactUs {
